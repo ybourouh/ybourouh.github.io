41d7910b5e33a2d4facc8c0b1532c8c0f068b006
Yanis.B <yanis.bourouh@gmail.com>
2026-07-02 12:59:31 +0200

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,48 @@
+<template>
+  <div style="margin-bottom: 80px;">
+    <h1>Hello!</h1>
+    
+    <div class="paragraph">
+      <div>
+        I'm <strong>Yanis Bourouh</strong>, a computer graphics student, game developper and artist.<br/>
+        I'm a very big fan of ....
+      </div>
+
+      <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">stuff</router-link>, on <router-link to="/other-projects">other stuff</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div>
+
+      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a monk, like my good friend John Rambo did a few years back. You can reach me at <a href="mailto:johnmatrix@deltaforce.us">johnmatrix@deltaforce.us</a> or <router-link to="/contact">through here</router-link>.</div>
+    
+    </div>
+    <div class="photo">
+      <img src="img/avatar.png" alt="Avatar of John" /> 
+    </div>
+
+    <div style="clear:both"></div>
+
+  </div>
+</template>
+
+<style scoped>
+.paragraph {
+  max-width: 700px;
+  margin-bottom: 20px;
+}
+
+.photo {
+  margin-top: 10px;
+  text-align: center;
+}
+
+@media only screen and (min-width: 620px){
+  .paragraph {
+    float: left;
+  }
+
+  .photo {
+    float: left;
+    padding: 10px;
+    padding-left: 80px;
+  }
+}
+
+</style>
